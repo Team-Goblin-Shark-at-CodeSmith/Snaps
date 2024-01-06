374b226265fed50e041bd5bd0310739e113abe55
ppost0 <p.post2321@yahoo.com>
2024-01-05 20:15:08 -0500

    Initial setup of workspace files

---
+++ webpack.config.js
@@ -0,0 +1,42 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+
+module.exports = {
+  mode: process.env.NODE_ENV,
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'public'),
+    filename: 'bundle.js',
+  },
+  devServer: {
+    port: "8080",
+    static: ["./public"],
+    open: true,
+    hot: true ,
+    liveReload: true,
+    proxy: {
+      // '/feed': 'http://localhost:3000'
+    }
+  },
+  module: {
+    rules: [
+      {
+          test: /\.(js|jsx)$/,    //kind of file extension this rule should look for and apply in test
+          exclude: /node_modules/, //folder to be excluded
+          use:  'babel-loader' //loader which we are going to use
+      },
+      {
+        test: /\.(png|jpe?g|gif)$/i,
+        use: [
+          {
+            loader: 'file-loader',
+          },
+        ]
+      }
+    ]
+  },
+  plugins: [new HtmlWebpackPlugin({
+    template: './template.html'
+  })]
+}
