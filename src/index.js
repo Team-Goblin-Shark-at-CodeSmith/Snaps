e43bc245fb692d37ab894aeb3d1ebaac03510369
ppost0 <p.post2321@yahoo.com>
2024-01-07 14:24:10 -0500

    Resolved React 17 issue regarding ReactDOM.createRoot()

--- src/index.js
+++ src/index.js
@@ -4,10 +4,12 @@ import ReactDOM from 'react-dom'
 import App from './App.jsx'
 import { store } from './store' //store.js
 import { Provider } from 'react-redux' // base thing needed for redux App
+import { createRoot } from "react-dom/client";
 
-ReactDOM.render(
+const container = document.getElementById('root');
+const root = createRoot(container);
+root.render(
   <Provider store={store}>
     <App />
-  </Provider>,
-  document.getElementById('root')
-)
+  </Provider>
+);
