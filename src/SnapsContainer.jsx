82fc6766b6e449a9a47eb54708edcb8dd450b816
ppost0 <p.post2321@yahoo.com>
2024-01-08 19:44:40 -0500

    Change styling prior to presentation

--- src/SnapsContainer.jsx
+++ src/SnapsContainer.jsx
@@ -45,7 +45,22 @@ const SnapsContainer = () => {
                   messages: [
                     {
                       role: "user",
-                      content: "give me a list of ten exotic foods",
+                      content: "Summarize this article into less than 300 words, respond in concise bullet points: Jacinda Ardern caused shockwaves around the globe with the January 2023 announcement of her resignation as New Zealand's prime minister. She cited not having 'enough in the tank'—as the motivation behind her decision. In 2017, she was the youngest woman ever to be elected prime minister at 37 years old. A year later, she became the second elected leader in modern history to give birth to a child during her term in office. Less than a month after Ardern announced her resignation, Nicola Sturgeon announced her own decision to step down as Scotland’s first minister. She cited concerns similar to those Ardern had expressed. “I am human. Politicians are human. We give all that we can for as long as we can. And then it’s time. And for me, it’s time.'\
+                      Following their resignations, many outlets raised questions about the larger implications of stepping down from these leadership positions. NPR pointed out that gender equality meant longer lives for everyone but that the decreasing numbers of women in top political positions suggest that equality is nowhere close to becoming a reality. The CEO Magazine focused on the pressures leaders face in general. From any perspective, the pressure we’re increasingly experiencing at work puts us all at risk of burnout, which is especially prominent when it comes to working parents. One 2022 survey from Ohio State University found 66% of working parents reported burnout. Burnout has also often been linked to depression and anxiety.\
+                      To fight against career burnout without having to tender a resignation, I suggest employees in all fields and at all levels adopt a multi-pronged strategy. 1. Focus on your physical and mental health. Burnout is a serious condition that can degrade physical and mental health. The flip side is also true: Defending against burnout requires a solid foundation of wellness in every sense of the word.\
+                      Getting adequate sleep is an important first step, as sleep deprivation can lead to higher degrees of stress, decreased energy and impaired attention. Good nutrition from healthy foods, if possible, is also important. Exercise, meditation, journaling and other wellness practices can all be a part of your approach.\
+                      The best tactics are the ones you can stick with consistently, so choose whatever aligns best with your preferences and circumstances.\
+                      2. Find ways to recharge your batteries outside of work.\
+                      One of the fastest routes to burnout runs straight through the 60-plus hour workweek. This starts with your choice of employment and workplace environment. Wherever possible, seek out positions with employers who respect work-life balance. Avoid accidentally falling into a lifestyle that fails to leave you space and time for pursuits you enjoy outside of work.\
+                      Making time for hobbies, social outings, etc., helps create a mental buffer around the work-related activities that can create pressure and scheduling demands that lead to burnout. Similarly, leave enough downtime for adequate rest in addition to your more active endeavors.\
+                      3. Reach out to a trusted friend or colleague.\
+                      Talking about the stress you’re experiencing can be an effective way to relieve the building pressure before it explodes; however, admitting that you’re feeling extreme stress can feel like a risky proposition. Think of two or three trusted friends and colleagues who are good listeners with whom you feel comfortable having a frank discussion about your situation.\
+                      4. Figure out a way to reduce the burden.\
+                      Sometimes, the only way to relieve rising work stress is to take items off your to-do list. Can you delegate any additional tasks to another team member? Of course, this may not be feasible at certain times. When your team is heavily involved with a big project, or you’re staring down an imminent deadline, you may have to find other solutions. But if it’s possible to lighten your workload, it’s an immediate way to free up bandwidth and fight burnout.\
+                      5. Seek professional help when appropriate.\
+                      Professional help can be extremely effective in many cases. Speaking with a counselor, psychologist or other mental health professional can help you alleviate the problem and learn strategies for reducing burnout before it becomes critical. Therapy may not always be the solution, but it’s an option worth considering.\
+                      Work-life balance may be a bit of a mythical unicorn these days, especially because burnout is sometimes the result of external, systemic factors that we can’t self-care our way out of. While we may not have complete control over all of the risk factors for burnout, aim to mitigate them wherever possible. Check in with yourself frequently and prioritize your mental and physical well-being. It’s much easier to take steps to prevent burnout than to recover from it after the fact.\
+                      ",
                     },
                   ],
                   temperature: 0.7,
@@ -56,13 +71,13 @@ const SnapsContainer = () => {
             const summary = content.choices[0].message.content.toString();
             const userUrlInput = document.getElementById('urlInput').value;
             const userTitleInput = document.getElementById('titleInput').value;
-
+              console.log("snapsList before post call ", snapsList)
             fetch('/my-snaps' , {
               method: 'POST',
               headers: {
                 'Content-Type': 'application/json',
              },
-              body: JSON.stringify({ user_id: 3, title: userTitleInput, url: userUrlInput, snap_text: summary })
+              body: JSON.stringify({ user_id: snapsList[0].user_id, title: userTitleInput, url: userUrlInput, snap_text: summary })
             })
             .then(res => res.json())
             .then(res => {
@@ -80,12 +95,13 @@ const SnapsContainer = () => {
 
     return(
         <div id="snaps-container">
-            <h1>My Snaps</h1>
+            <img id="snaps-page-logo" src="./images/snaps-page-logo.png"/>
+            {/* <h1>My Snaps</h1> */}
             <table className="snaps-table container">
                 {snapsComponentList}
             </table>
-            <input type='title' id='titleInput' placeholder='Enter a title...'/>
-            <input type='url' id='urlInput' placeholder='Enter a URL...'/>
+            <input type='title' className='snaps-input' id='titleInput' placeholder='Enter a title...'/>
+            <input type='url' className='snaps-input'  id='urlInput' placeholder='Enter a URL...'/>
             <button id='add-button'onClick={handleAdd}>Add</button>
         </div>
     );
@@ -98,19 +114,3 @@ const SnapsContainer = () => {
 export default SnapsContainer;
 
 
-// "Summarize this article into less than 300 words: Jacinda Ardern caused shockwaves around the globe with the January 2023 announcement of her resignation as New Zealand's prime minister. She cited not having 'enough in the tank'—as the motivation behind her decision. In 2017, she was the youngest woman ever to be elected prime minister at 37 years old. A year later, she became the second elected leader in modern history to give birth to a child during her term in office. Less than a month after Ardern announced her resignation, Nicola Sturgeon announced her own decision to step down as Scotland’s first minister. She cited concerns similar to those Ardern had expressed. “I am human. Politicians are human. We give all that we can for as long as we can. And then it’s time. And for me, it’s time.'\
-//                       Following their resignations, many outlets raised questions about the larger implications of stepping down from these leadership positions. NPR pointed out that gender equality meant longer lives for everyone but that the decreasing numbers of women in top political positions suggest that equality is nowhere close to becoming a reality. The CEO Magazine focused on the pressures leaders face in general. From any perspective, the pressure we’re increasingly experiencing at work puts us all at risk of burnout, which is especially prominent when it comes to working parents. One 2022 survey from Ohio State University found 66% of working parents reported burnout. Burnout has also often been linked to depression and anxiety.\
-//                       To fight against career burnout without having to tender a resignation, I suggest employees in all fields and at all levels adopt a multi-pronged strategy. 1. Focus on your physical and mental health. Burnout is a serious condition that can degrade physical and mental health. The flip side is also true: Defending against burnout requires a solid foundation of wellness in every sense of the word.\
-//                       Getting adequate sleep is an important first step, as sleep deprivation can lead to higher degrees of stress, decreased energy and impaired attention. Good nutrition from healthy foods, if possible, is also important. Exercise, meditation, journaling and other wellness practices can all be a part of your approach.\
-//                       The best tactics are the ones you can stick with consistently, so choose whatever aligns best with your preferences and circumstances.\
-//                       2. Find ways to recharge your batteries outside of work.\
-//                       One of the fastest routes to burnout runs straight through the 60-plus hour workweek. This starts with your choice of employment and workplace environment. Wherever possible, seek out positions with employers who respect work-life balance. Avoid accidentally falling into a lifestyle that fails to leave you space and time for pursuits you enjoy outside of work.\
-//                       Making time for hobbies, social outings, etc., helps create a mental buffer around the work-related activities that can create pressure and scheduling demands that lead to burnout. Similarly, leave enough downtime for adequate rest in addition to your more active endeavors.\
-//                       3. Reach out to a trusted friend or colleague.\
-//                       Talking about the stress you’re experiencing can be an effective way to relieve the building pressure before it explodes; however, admitting that you’re feeling extreme stress can feel like a risky proposition. Think of two or three trusted friends and colleagues who are good listeners with whom you feel comfortable having a frank discussion about your situation.\
-//                       4. Figure out a way to reduce the burden.\
-//                       Sometimes, the only way to relieve rising work stress is to take items off your to-do list. Can you delegate any additional tasks to another team member? Of course, this may not be feasible at certain times. When your team is heavily involved with a big project, or you’re staring down an imminent deadline, you may have to find other solutions. But if it’s possible to lighten your workload, it’s an immediate way to free up bandwidth and fight burnout.\
-//                       5. Seek professional help when appropriate.\
-//                       Professional help can be extremely effective in many cases. Speaking with a counselor, psychologist or other mental health professional can help you alleviate the problem and learn strategies for reducing burnout before it becomes critical. Therapy may not always be the solution, but it’s an option worth considering.\
-//                       Work-life balance may be a bit of a mythical unicorn these days, especially because burnout is sometimes the result of external, systemic factors that we can’t self-care our way out of. While we may not have complete control over all of the risk factors for burnout, aim to mitigate them wherever possible. Check in with yourself frequently and prioritize your mental and physical well-being. It’s much easier to take steps to prevent burnout than to recover from it after the fact.\
-//                       "
