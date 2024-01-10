ce8d78d55aa588e6e1962f2dad174b9a9c6f8c20
thomas444ortiz <thomasortiz444@gmail.com>
2024-01-10 10:45:19 -0500

    commit

--- client/App.jsx
+++ client/App.jsx
@@ -3,20 +3,19 @@ import LoginContainer from "./containers/LoginContainer.jsx";
 import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import './stylesheets/styles.css';
 import SnapsContainer from "./containers/SnapsContainer.jsx";
+import Navbar from "./containers/SnapsContainer.jsx";
 
 const App = () => {
 
     return(
-
-        <Router>
-            <Routes>
-                <Route path="/" element={<LoginContainer />} />
-                <Route path="/snaps" element={<SnapsContainer />} />
-            </Routes>
-        </Router>
-
-
-
+        <div>
+            <Router>
+                <Routes>
+                    <Route path="/" element={<LoginContainer />} />
+                    <Route path="/snaps" element={<SnapsContainer />} />
+                </Routes>
+            </Router>
+        </div>
     )
 }
 
---
+++ client/components/Navbar.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+function Navbar() {
+
+  return (
+    <h1>Navbar</h1>
+  );
+}
+
+export default Navbar;
