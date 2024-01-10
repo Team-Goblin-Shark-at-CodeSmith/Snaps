3d85627183c7ae25a2ed968dfffa1dd0e11c9753
thomas444ortiz <thomasortiz444@gmail.com>
2024-01-09 20:13:42 -0500

    implement hash

--- client/App.jsx
+++ client/App.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import LoginContainer from "./containers/LoginContainer.jsx";
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import './stylesheets/styles.css';
 import SnapsContainer from "./containers/SnapsContainer.jsx";
 
