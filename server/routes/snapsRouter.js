2fff7fd1f0e281d5f731bda6b1599db2f2d65a91
Matt Hart <mhart1992@gmail.com>
2024-01-11 17:31:58 -0500

    Users can now add snaps to their account -tracked by cookies being added on login middleware. User's snaps also load when a new snap is added

--- server/routes/snapsRouter.js
+++ server/routes/snapsRouter.js
@@ -4,20 +4,21 @@ const router = express.Router();
 
 const snapsController = require('../controllers/snapsController');
 
-router.get('/', 
-  snapsController.getSnaps, 
-  (req,res) => {
+router.get('/',
+  snapsController.getSnaps,
+  (req, res) => {
     return res.status(200).json(res.locals.allSnaps);
-});
+  });
 
 
-router.post('/', 
-snapsController.scrapeWeb, 
-snapsController.makeApiCall, 
-snapsController.addSnap, 
-(req,res) => {
-  return res.status(200).json(res.locals.allSnaps);
-});
+router.post('/',
+  snapsController.scrapeWeb,
+  snapsController.makeApiCall,
+  snapsController.addSnap,
+  (req, res) => {
+    // redirect('/my-snaps');
+    return res.status(200).json(res.locals.allSnaps);
+  });
 
 
 module.exports = router;
